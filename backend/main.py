e983b4bbeab6b2776700474468fb9d998c1e9571
Santhosh Kumar <santhoshsk2323@gmail.com>
2026-09-19 10:03:15 +0530

    added API service link

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "https://verifit.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
